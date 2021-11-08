2a9ad7d31199280aaeab2824ebe154639a79289e
IsmailElShinnawy <ismailelshennawy@gmail.com>
2021-11-08 19:55:05 +0200

    added admin and token authentication to search and view routes

--- backend/components/flight/flight.routes.js
+++ backend/components/flight/flight.routes.js
@@ -11,9 +11,9 @@ const { isAdmin, verifyToken } = require('../../middleware/authJwt');
 const router = express.Router();
 
 router.post('/', [verifyToken, isAdmin], create);
-router.get('/', view);
+router.get('/', [verifyToken, isAdmin], view);
 router.put('/:id', [verifyToken, isAdmin], updateFlight);
-router.post('/search', searchFlights);
+router.post('/search', [verifyToken, isAdmin], searchFlights);
 router.delete('/', [verifyToken, isAdmin], deleteFlight);
 
 module.exports = router;
