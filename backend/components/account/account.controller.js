a79a6b2ae22818b2c070ea814f61f33e404c7c64
Nadinefouda21 <rostom_80@yahoo.com>
2021-11-25 12:51:54 +0200

    User is allowed to update his/her accoount

--- backend/components/account/account.controller.js
+++ backend/components/account/account.controller.js
@@ -37,4 +37,31 @@ exports.updateUserProfile = async (req, res) => {
   } catch (err) {
     res.status(500).json({ status: 'fail', message: err });
   }
+  exports.createUser = async (req, res) => {
+    const {
+      email,
+      password,
+      username,
+      firstname,
+      lastname,
+      passportNumber,
+      roles,
+    } = req.body;
+    try {
+      const u = new User({
+        email: email,
+        password: password,
+        username: username,
+        firstname: firstname,
+        lastname: lastname,
+        passportNumber: passportNumber,
+        roles: roles,
+    
+      });
+      const result = await u.save();
+      res.status(200).json({ status: 'success', data: result });
+    } catch (err) {
+      res.status(500).json({ status: 'fail', message: err });
+    }
+  };
 };
