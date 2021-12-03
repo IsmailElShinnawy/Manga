c2daf5ccbfceab8af12dceed2a75b93a7d8ed006
IsmailElShinnawy <ismailelshennawy@gmail.com>
2021-12-03 15:18:55 +0200

    user able to choose departure and return seats

--- frontend/src/components/flights/CheckoutPage.jsx
+++ frontend/src/components/flights/CheckoutPage.jsx
@@ -4,14 +4,67 @@ import { useHttpClient } from '../../hooks/http-hook';
 import Loading from '../shared/UIKit/Loading';
 
 const CheckoutPage = () => {
-  const { departureFlight, returnFlight, departureFlightCabin, returnFlightCabin } =
-    useReservation();
+  const {
+    departureFlight,
+    returnFlight,
+    departureFlightCabin,
+    returnFlightCabin,
+    departureFlightPassengers,
+    returnFlightPassengers,
+  } = useReservation();
   const { sendRequest: sendDepartureRequest, isLoading: isDepartureLoading } =
     useHttpClient();
   const { sendRequest: sendReturnRequest, isLoading: isReturnLoading } = useHttpClient();
 
   const [departureFlightAvailableSeats, setDepartureFlightAvailableSeats] = useState([]);
   const [returnFlightAvailableSeats, setReturnFlightAvailableSeats] = useState([]);
+
+  const [chosenDepartureSeats, setChosenDepartureSeats] = useState([]);
+  const [chosenReturnSeats, setChosenReturnSeats] = useState([]);
+
+  const addDepartureSeat = seat => {
+    setChosenDepartureSeats(seats => [...seats, seat]);
+  };
+
+  const removeDepartureSeat = seat => {
+    setChosenDepartureSeats(seats => seats.filter(s => s !== seat));
+  };
+
+  const addReturnSeat = seat => {
+    setChosenReturnSeats(seats => [...seats, seat]);
+  };
+
+  const removeReturnSeat = seat => {
+    setChosenReturnSeats(seats => seats.filter(s => s !== seat));
+  };
+
+  const isDepartureSeat = seat => {
+    const idx = chosenDepartureSeats.findIndex(s => s === seat);
+    return idx >= 0;
+  };
+
+  const isReturnSeat = seat => {
+    const idx = chosenReturnSeats.findIndex(s => s === seat);
+    return idx >= 0;
+  };
+
+  const toggleDepartureSeat = seat => {
+    if (isDepartureSeat(seat)) {
+      removeDepartureSeat(seat);
+    } else {
+      if (chosenDepartureSeats.length === departureFlightPassengers) return;
+      addDepartureSeat(seat);
+    }
+  };
+
+  const toggleReturnSeat = seat => {
+    if (isReturnSeat(seat)) {
+      removeReturnSeat(seat);
+    } else {
+      if (chosenReturnSeats.length === returnFlightPassengers) return;
+      addReturnSeat(seat);
+    }
+  };
 
   useEffect(() => {
     const fetchDepartureFlightSeats = async () => {
@@ -64,14 +117,21 @@ const CheckoutPage = () => {
             Step 1 - Choosing your seats
           </h2>
           <h3 className='text-lg font-medium font-nunito leading-4 text-grey-secondary mb-4'>
-            Departure flight seats
+            Departure flight seats{' '}
+            <span className='italic text-sm'>(choose {departureFlightPassengers})</span>
           </h3>
           {!isDepartureLoading ? (
             <div className='flex w-full flex-wrap'>
               {departureFlightAvailableSeats.map(
                 (seat, idx) =>
                   seat && (
-                    <span className='mr-3 mb-3 w-10 h-10 flex justify-center items-center rounded-full bg-pale-purple hover:cursor-pointer'>
+                    <span
+                      onClick={() => toggleDepartureSeat(idx)}
+                      className={`mr-3 mb-3 w-10 h-10 flex justify-center items-center rounded-full ${
+                        isDepartureSeat(idx) ? 'bg-primary text-white' : 'bg-pale-purple'
+                      } hover:cursor-pointer`}
+                      key={`departureSeat${idx}`}
+                    >
                       {departureFlightCabin === 'business' ? 'B' : 'E'}
                       {idx}
                     </span>
@@ -84,14 +144,21 @@ const CheckoutPage = () => {
             </div>
           )}
           <h3 className='text-lg font-medium font-nunito leading-4 text-grey-secondary mb-4'>
-            Return flight seats
+            Return flight seats{' '}
+            <span className='italic text-sm'>(choose {returnFlightPassengers})</span>
           </h3>
           {!isReturnLoading ? (
             <div className='flex w-full flex-wrap'>
               {returnFlightAvailableSeats.map(
                 (seat, idx) =>
                   seat && (
-                    <span className='mr-3 mb-3 w-10 h-10 flex justify-center items-center rounded-full bg-pale-purple hover:cursor-pointer'>
+                    <span
+                      onClick={() => toggleReturnSeat(idx)}
+                      className={`mr-3 mb-3 w-10 h-10 flex justify-center items-center rounded-full ${
+                        isReturnSeat(idx) ? 'bg-primary text-white' : 'bg-pale-purple'
+                      } hover:cursor-pointer`}
+                      key={`returnSeat${idx}`}
+                    >
                       {returnFlightCabin === 'business' ? 'B' : 'E'}
                       {idx}
                     </span>
