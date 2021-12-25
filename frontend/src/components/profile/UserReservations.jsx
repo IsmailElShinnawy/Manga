8e2437fa78b4892c2b1a318686d870f8f3fa1a66
IsmailElShinnawy <ismailelshennawy@gmail.com>
2021-12-25 23:19:03 +0200

    added illlustration in profile for no reservations

--- frontend/src/components/profile/UserReservations.jsx
+++ frontend/src/components/profile/UserReservations.jsx
@@ -8,6 +8,7 @@ import Modal from '../shared/Modal/Modal';
 import { ReactComponent as IconEdit } from '../../assets/icons/IconEdit.svg';
 import { ReactComponent as IconRefresh } from '../../assets/icons/IconRefresh.svg';
 import { ReactComponent as IconMail } from '../../assets/icons/IconMail.svg';
+import NoResults from '../../assets/images/no-results.png';
 import EditReservationSeatsModal from './EditReservationSeatsModal';
 import UpdateFlightModal from './UpdateFlightModal';
 
@@ -120,110 +121,117 @@ const UserReservations = () => {
       <EditReservationSeatsModal close={handleCloseEdit} editing={editing} />
       <UpdateFlightModal close={handleCloseUpdate} updating={updating} />
       <div>
-        {reservations?.map(reservation => {
-          return (
-            <div key={reservation._id}>
-              <div className='flex items-center'>
-                <h2 className='font-nunito font-semibold text-lg leading-6 text-grey-primary mb-4 mr-auto'>
-                  Order #{reservation._id.toUpperCase()}{' '}
-                  <Link
-                    to={`/itinerary/${reservation._id}`}
-                    className='text-primary text-base leading-5 font-bold'
-                  >
-                    View summary
-                  </Link>
-                </h2>
-                <div className='px-2 mr-2'>
-                  <Button
-                    disabled={isLoading}
-                    onClick={() => handleSubmit(reservation?._id)}
-                    icon={<IconMail fill='#605DEC' />}
-                  />
-                </div>
-                <div>
-                  <Button
-                    text='Cancel'
-                    onClick={() => {
-                      setToBeCancelled(reservation._id);
-                      setShowConfirmModal(true);
-                    }}
-                    isLoading={isDeleting}
-                    loadingText='Cancelling...'
-                    disabled={isDeleting}
-                  />
-                </div>
-              </div>
-              <div className='mb-4 flex'>
-                <FlightCard
-                  arrivalTerminal={reservation?.departureFlight.arrivalTerminal}
-                  arrivalTime={reservation?.departureFlight.arrivalTime}
-                  departureTerminal={reservation?.departureFlight.departureTerminal}
-                  departureTime={reservation?.departureFlight.departureTime}
-                  flightNumber={reservation?.departureFlight.flightNumber}
-                  id={reservation?.departureFlight._id}
-                  noHover
-                  noSeats
-                  price={reservation?.departureFlight.ticketPrice}
-                  baggageAllowance={reservation?.departureFlight.baggageAllowance}
-                />
+        {reservations?.length > 0 ? (
+          reservations?.map(reservation => {
+            return (
+              <div key={reservation._id}>
                 <div className='flex items-center'>
-                  <Button
-                    icon={<IconEdit fill='#605DEC' />}
-                    onClick={() => handleEdit(reservation._id, 'departure')}
-                  />
+                  <h2 className='font-nunito font-semibold text-lg leading-6 text-grey-primary mb-4 mr-auto'>
+                    Order #{reservation._id.toUpperCase()}{' '}
+                    <Link
+                      to={`/itinerary/${reservation._id}`}
+                      className='text-primary text-base leading-5 font-bold'
+                    >
+                      View summary
+                    </Link>
+                  </h2>
+                  <div className='px-2 mr-2'>
+                    <Button
+                      disabled={isLoading}
+                      onClick={() => handleSubmit(reservation?._id)}
+                      icon={<IconMail fill='#605DEC' />}
+                    />
+                  </div>
+                  <div>
+                    <Button
+                      text='Cancel'
+                      onClick={() => {
+                        setToBeCancelled(reservation._id);
+                        setShowConfirmModal(true);
+                      }}
+                      isLoading={isDeleting}
+                      loadingText='Cancelling...'
+                      disabled={isDeleting}
+                    />
+                  </div>
                 </div>
-                <div className='flex items-center ml-2'>
-                  <Button
-                    icon={<IconRefresh fill='#605DEC' />}
-                    onClick={() =>
-                      handleUpdate(
-                        reservation._id,
-                        'departure',
-                        reservation?.departureFlight.ticketPrice,
-                        reservation?.departureFlightSeats.length,
-                        reservation?.departureFlightCabin
-                      )
-                    }
+                <div className='mb-4 flex'>
+                  <FlightCard
+                    arrivalTerminal={reservation?.departureFlight.arrivalTerminal}
+                    arrivalTime={reservation?.departureFlight.arrivalTime}
+                    departureTerminal={reservation?.departureFlight.departureTerminal}
+                    departureTime={reservation?.departureFlight.departureTime}
+                    flightNumber={reservation?.departureFlight.flightNumber}
+                    id={reservation?.departureFlight._id}
+                    noHover
+                    noSeats
+                    price={reservation?.departureFlight.ticketPrice}
+                    baggageAllowance={reservation?.departureFlight.baggageAllowance}
                   />
+                  <div className='flex items-center'>
+                    <Button
+                      icon={<IconEdit fill='#605DEC' />}
+                      onClick={() => handleEdit(reservation._id, 'departure')}
+                    />
+                  </div>
+                  <div className='flex items-center ml-2'>
+                    <Button
+                      icon={<IconRefresh fill='#605DEC' />}
+                      onClick={() =>
+                        handleUpdate(
+                          reservation._id,
+                          'departure',
+                          reservation?.departureFlight.ticketPrice,
+                          reservation?.departureFlightSeats.length,
+                          reservation?.departureFlightCabin
+                        )
+                      }
+                    />
+                  </div>
+                </div>
+                <div className='mb-10 flex'>
+                  <FlightCard
+                    arrivalTerminal={reservation?.returnFlight.arrivalTerminal}
+                    arrivalTime={reservation?.returnFlight.arrivalTime}
+                    departureTerminal={reservation?.returnFlight.departureTerminal}
+                    departureTime={reservation?.returnFlight.departureTime}
+                    flightNumber={reservation?.returnFlight.flightNumber}
+                    id={reservation?.returnFlight._id}
+                    noHover
+                    noSeats
+                    price={reservation?.returnFlight.ticketPrice}
+                    baggageAllowance={reservation?.returnFlight.baggageAllowance}
+                  />
+                  <div className='flex items-center'>
+                    <Button
+                      icon={<IconEdit fill='#605DEC' />}
+                      onClick={() => handleEdit(reservation._id, 'return')}
+                    />
+                  </div>
+                  <div className='flex items-center ml-2'>
+                    <Button
+                      icon={<IconRefresh fill='#605DEC' />}
+                      onClick={() =>
+                        handleUpdate(
+                          reservation._id,
+                          'return',
+                          reservation?.returnFlight.ticketPrice,
+                          reservation?.returnFlightSeats.length,
+                          reservation?.returnFlightCabin
+                        )
+                      }
+                    />
+                  </div>
                 </div>
               </div>
-              <div className='mb-10 flex'>
-                <FlightCard
-                  arrivalTerminal={reservation?.returnFlight.arrivalTerminal}
-                  arrivalTime={reservation?.returnFlight.arrivalTime}
-                  departureTerminal={reservation?.returnFlight.departureTerminal}
-                  departureTime={reservation?.returnFlight.departureTime}
-                  flightNumber={reservation?.returnFlight.flightNumber}
-                  id={reservation?.returnFlight._id}
-                  noHover
-                  noSeats
-                  price={reservation?.returnFlight.ticketPrice}
-                  baggageAllowance={reservation?.returnFlight.baggageAllowance}
-                />
-                <div className='flex items-center'>
-                  <Button
-                    icon={<IconEdit fill='#605DEC' />}
-                    onClick={() => handleEdit(reservation._id, 'return')}
-                  />
-                </div>
-                <div className='flex items-center ml-2'>
-                  <Button
-                    icon={<IconRefresh fill='#605DEC' />}
-                    onClick={() =>
-                      handleUpdate(
-                        reservation._id,
-                        'return',
-                        reservation?.returnFlight.ticketPrice,
-                        reservation?.returnFlightSeats.length,
-                        reservation?.returnFlightCabin
-                      )
-                    }
-                  />
-                </div>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className='w-full flex flex-col opacity-50 justify-center items-center'>
+            <img src={NoResults} width='25%' height='25%' alt='No reservations' />
+            <p>It seams that you did not reserve any flights yet!</p>
+          </div>
+        )}
       </div>
     </>
   );
